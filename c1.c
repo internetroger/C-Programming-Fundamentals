3605e22a388e4af805b875b9dfe2ee7260bae4b3
Roger Robertson Jr <rjrjr@pm.me>
2020-08-18 10:53:26 -0700

    initial couple exercises

---
+++ c1.c
@@ -0,0 +1,8 @@
+#import <stdio.h>
+#import <stdlib.h>
+
+int main() 
+{
+    printf("Hello Roger Robertson!\n");
+    return 0;
+}
---
+++ ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    // every command ends with a semicolon
+    // comments use //
+    printf("Hello World!\n");
+    return 0;
+}
